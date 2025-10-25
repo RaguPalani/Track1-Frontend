b7216bd8d2d875d7f23eebab6e7a72c166acd85e
RaguPalani <144354878+RaguPalani@users.noreply.github.com>
2025-10-25 19:33:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,9 @@ function App() {
 
   return (
     <Router>
-      <div className="min-h-screen bg-background">
+      <div className="bg-background w-4/5 mx-auto">
         <Header />
-        <main className="container mx-auto px-4 py-8 bg-background w-4/5 mx-auto">
+        <main className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<Search />} />
